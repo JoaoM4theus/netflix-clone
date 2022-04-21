b302fb4c8df7bb8c5ed2a52f25e8f5095d2d17ad
Joao Matheus <joao.rocha@corebiz.ag>
2022-04-21 10:57:05 -0300

    Implementing rootViewControllers with views necessary

--- Netflix-Clone/MainTabBarViewController.swift
+++ Netflix-Clone/MainTabBarViewController.swift
@@ -11,6 +11,13 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        
+        let home = UINavigationController(rootViewController: HomeViewController())
+        let upcoming = UINavigationController(rootViewController: UpcomingViewController())
+        let search = UINavigationController(rootViewController: SearchViewController())
+        let download = UINavigationController(rootViewController: DownloadViewController())
+        
+        setViewControllers([home, upcoming, search, download], animated: true)
     }
 }
 
