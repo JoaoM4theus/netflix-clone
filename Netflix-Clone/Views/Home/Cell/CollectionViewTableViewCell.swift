dd0039a5ab6fa4ac9abbe5b0b2a1ed93f1a97484
Joao Matheus <joao.rocha@corebiz.ag>
2022-05-04 20:38:34 -0300

    Creating header from TableView.

--- Netflix-Clone/Views/Home/Cell/CollectionViewTableViewCell.swift
+++ Netflix-Clone/Views/Home/Cell/CollectionViewTableViewCell.swift
@@ -13,6 +13,7 @@ class CollectionViewTableViewCell: UITableViewCell {
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 140, height: 200)
         let collectionView = UICollectionView(frame: .zero,
                                               collectionViewLayout: layout)
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
