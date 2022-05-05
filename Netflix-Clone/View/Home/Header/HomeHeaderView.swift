3d43f789d80154bc95f68196c1852f0580aecf01
Joao Matheus <joao.rocha@corebiz.ag>
2022-05-05 19:27:17 -0300

    Organizing project.

---
+++ Netflix-Clone/View/Home/Header/HomeHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeHeaderView.swift
+//  Netflix-Clone
+//
+//  Created by Joao Matheus on 05/05/22.
+//
+
+import UIKit
+
+class HomeHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
