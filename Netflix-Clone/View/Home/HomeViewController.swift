bdf975370655a1e997085ea0d189ec68909486d3
Joao Matheus <joao.rocha@corebiz.ag>
2022-05-29 20:26:18 -0300

    Implementing scroll view to hide navigation bar when pull down.

--- Netflix-Clone/View/Home/HomeViewController.swift
+++ Netflix-Clone/View/Home/HomeViewController.swift
@@ -82,4 +82,11 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return 40
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y - defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: .zero, y: min(0, -offset))
+    }
+    
 }
